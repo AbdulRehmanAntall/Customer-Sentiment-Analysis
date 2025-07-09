bf86934efadc347eeb517163a4a45e4e7585671e
AbdulRehmanAntall <abdulrehmanantall420@gmail.com>
2025-07-09 09:43:28 +0500

    second commit

---
+++ frontend/Pages/AudioRecord.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './AudioRecord.css'
+const AudioRecord = () => {
+    return (
+        <div>
+            this is audio record page
+        </div>
+    )
+}
+
+export default AudioRecord
---
+++ frontend/Pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Landing.css';
+const Landing = () => {
+    return (
+        <div>
+            this is landing page
+        </div>
+    )
+}
+
+export default Landing
